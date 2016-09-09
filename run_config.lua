86c57a98b3e23b6248a6785863f74c75655a8379
davidp <davidp@huelvayork.com>
2016-09-09 19:23:14 +0200

    reorganize code; launch setup_server from timer

--- run_config.lua
+++ run_config.lua
@@ -1,9 +1,88 @@
 -- compile this module if you have memory issues
 
+
+function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+
+function setup_server()
+
+     print("Setting up Wifi AP")
+     local cfg={}
+     cfg.ssid = "ESPconfig"
+     cfg.pwd  = "12345678"
+
+     wifi.setmode(wifi.SOFTAP)
+     wifi.ap.config(cfg)
+
+     -- Prepare HTML form
+     print("Preparing HTML Form")
+     if (file.open('configform.html','r')) then
+          buf = file.read()
+          file.close()
+     end
+     -- interpret variables in strings
+     -- ssid="WLAN01"
+     -- str="my ssid is ${ssid}"
+     -- interp(str) -> "my ssid is WLAN01"
+     buf = buf:gsub('($%b{})', function(w) 
+               return _G[w:sub(3, -2)] or "" 
+          end)
+
+     print("Setting up webserver")
+     srv=net.createServer(net.TCP)
+     srv:listen(80, handle_connection)
+     print (node.heap())
+     print("Setting up Webserver done. Please connect to: " .. wifi.ap.getip())
+
+end
+
+function handle_connection(conn)
+    conn:on("receive", function(client,request)
+        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+        if(method == nil)then
+            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+         end
+        local _GET = {}
+        if (vars ~= nil)then
+            for k, v in string.gmatch(vars, "([_%w]+)=([^%&]+)&*") do
+                _GET[k] = unescape(v)
+                print(v)
+            end
+        end
+          
+        if (_GET.password ~= nil and _GET.ssid ~= nil) then
+            if (_GET.ssid == "-1") then _GET.ssid=_GET.hiddenssid end
+            client:send("Saving data..");
+            file.open("config.lua", "w")
+            file.writeline('ssid = "' .. _GET.ssid .. '"')
+            file.writeline('password = "' .. _GET.password .. '"')
+            -- write every variable in the form 
+           for k,v in pairs(_GET) do
+                file.writeline(k..' = "'..v ..'"')
+                print (k..' = '..v)
+           end 
+            file.close()
+            node.compile("config.lua")
+            file.remove("config.lua")
+            client:send(buf);
+         node.restart();
+        end
+    
+      payloadLen = string.len(buf)
+      client:send("HTTP/1.1 200 OK\r\n"
+        .."Content-Type    text/html; charset=UTF-8\r\n"
+        .."Content-Length:" .. tostring(payloadLen) .. "\r\n"
+        .."Connection:close\r\n\r\n"
+        ..buf, function(client) client:close() end);
+      end)
+end
+
 -- GPIO0 resets the module
 gpio.mode(3, gpio.INT)
 gpio.trig(3,"both",function()
-          node.restart()
+           --node.restart()
      end)
      
 -- read previous config
@@ -20,6 +99,7 @@ local unescape = function (s)
      return s
 end
 
+
 print("Get available APs")
 wifi.setmode(wifi.STATION) 
 wifi.sta.getap(function(t)
@@ -33,79 +113,10 @@ wifi.sta.getap(function(t)
                count = count+1
                if (count>=10) then break end
 		end 
-          available_aps = available_aps .. "<option value='-1'>---hidden SSID---</option>"
-          setup_server()
+        available_aps = available_aps .. "<option value='-1'>---hidden SSID---</option>"
 	end
 end)
 
-function setup_server()
 
-     -- Prepare HTML form
-     print("Preparing HTML Form")
-     if (file.open('configform.html','r')) then
-          buf = file.read()
-          file.close()
-     end
-     -- interpret variables in strings
-     -- ssid="WLAN01"
-     -- str="my ssid is ${ssid}"
-     -- interp(str) -> "my ssid is WLAN01"
-     buf = buf:gsub('($%b{})', function(w) 
-               return _G[w:sub(3, -2)] or "" 
-          end)
-
-	print("Setting up Wifi AP")
-     wifi.setmode(wifi.SOFTAP)
-	local cfg={}
-	cfg.ssid = "ESPconfig"
-	cfg.pwd  = ""
-	wifi.ap.config(cfg)
-
-	print("Setting up webserver")
-	local srv = nil
-	srv=net.createServer(net.TCP)
-	srv:listen(80,function(conn)
-    	conn:on("receive", function(client,request)
-        	local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
-        	if(method == nil)then
-	            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
-    	     end
-        	local _GET = {}
-        	if (vars ~= nil)then
-            	for k, v in string.gmatch(vars, "([_%w]+)=([^%&]+)&*") do
-                	_GET[k] = unescape(v)
-            	end
-	        end
-              
-    	    if (_GET.password ~= nil and _GET.ssid ~= nil) then
-          if (_GET.ssid == "-1") then _GET.ssid=_GET.hiddenssid end
-    	    	client:send("Saving data..");
-        		file.open("config.lua", "w")
-				file.writeline('ssid = "' .. _GET.ssid .. '"')
-				file.writeline('password = "' .. _GET.password .. '"')
-               -- write every variable in the form 
-               for k,v in pairs(_GET) do
-                    file.writeline(k..' = "'..v ..'"')
-               end 
-    			file.close()
-    			node.compile("config.lua")
-    			file.remove("config.lua")
-    			client:send(buf);
-		     node.restart();
-        	end
-
-          payloadLen = string.len(buf)
-          client:send("HTTP/1.1 200 OK\r\n")
-          client:send("Content-Type    text/html; charset=UTF-8\r\n")
-            
-          client:send("Content-Length:" .. tostring(payloadLen) .. "\r\n")
-          client:send("Connection:close\r\n\r\n")               
-          client:send(buf, function(client) client:close() end);
-          end)
-	end)
-	print("Setting up Webserver done. Please connect to: " .. wifi.ap.getip())
-end
-
-function trim(s)
-  return (s:gsub("^%s*(.-)%s*$", "%1"))
-end
+tmr.register(0, 5000, tmr.ALARM_SINGLE, setup_server)
+tmr.start(0)
